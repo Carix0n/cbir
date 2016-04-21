5c914c9a74efc220cb2ebb66e25d3edf048ca22d
Pavel Danilov <xzergx94@gmail.com>
2016-04-21 20:12:27 +0300

    Matlab version commit with fixes

--- src/test/python2/suite.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestAdditionalFunctions(unittest.TestCase):
-    def test_smth(self):
-        self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
